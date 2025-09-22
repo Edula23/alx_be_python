8f28004011b88d124814c96df308f6ae316132a0
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-09-22 11:21:12 +0300

    Change pattern_drawing.py: remove space between asteriskx

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,6 @@ size = int(input("Enter the size of the pattern:"))
 i = size
 while i > 0:
     for j in range(size):
-        print("*", end=" ")        
+        print("*", end="")        
     print()    
     i -= 1
