07f8b3060e661209a1749da430e48b9ce02b6387
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-09-22 11:20:21 +0300

    Change pattern_drawing.py: use nested loop

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,7 @@
 size = int(input("Enter the size of the pattern:"))
 i = size
 while i > 0:
-    print("*" * size, end="")
-    print()
+    for j in range(size):
+        print("*", end=" ")        
+    print()    
     i -= 1
