7b0bee7bd031db674ac6a2f143e4b01caaff065c
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-10-06 02:10:03 +0300

    float division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,4 +6,4 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
     except ValueError:
         return "Error: Please enter numeric values only."
-    return f"The result of the division is: {result}"
+    return f"The result of the division is {result}"
