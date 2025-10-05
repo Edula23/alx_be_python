d57c660e3f92fb09afded6cebd69648a09c62530
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-10-06 01:20:25 +0300

    add print statement

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,4 +14,4 @@ class BankAccount:
             return True
 
     def display_balance(self):
-        print(f"Current balance: {self.account_balance}")
+        return (f"Current balance: {self.account_balance}")
