21116980278cfaf597efe52781408f1aab331811
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-10-06 02:47:26 +0300

    float division calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,20 +4,20 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calc.test_addition(2, 3), 5)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
 
-    def test_subtract(self):
-        self.assertEqual(self.calc.test_subtraction(5, 3), 2)
-        self.assertEqual(self.calc.test_subtraction(0, 0), 0)
-        self.assertEqual(self.calc.test_subtraction(-1, -1), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.test_multiplication(4, 5), 20)
-        self.assertEqual(self.calc.test_multiplication(-1, 1), -1)
-        self.assertEqual(self.calc.test_multiplication(-2, -3), 6)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-2, -3), 6)
 
-    def test_divide(self):
-        self.assertEqual(self.calc.test_division(10, 2), 5)
-        self.assertEqual(self.calc.test_division(5, 0), None)  # Division by zero
-        self.assertEqual(self.calc.test_division(-6, -2), 3)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, 0), None)  # Division by zero
+        self.assertEqual(self.calc.divide(-6, -2), 3)
